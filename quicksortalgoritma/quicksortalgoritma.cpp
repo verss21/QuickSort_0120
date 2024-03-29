fe029b60a022dc5acaafc22f7fc577af6f757154
verss21 <arjunafrizal01@gmail.com>
2024-03-29 10:39:02 +0700

    step 12

--- quicksortalgoritma/quicksortalgoritma.cpp
+++ quicksortalgoritma/quicksortalgoritma.cpp
@@ -82,3 +82,6 @@ void q_short(int low, int high)
 		//swap the pivot element with the element at index j
 		swap(low, j);
 	}
+
+	//recursive call to sort the left sub array
+	q_short(low, j - 1); //step 12
