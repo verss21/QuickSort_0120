5733a869837a2c156e93683d71b700ed64eba56e
verss21 <arjunafrizal01@gmail.com>
2024-03-29 10:36:46 +0700

    step 11

--- quicksortalgoritma/quicksortalgoritma.cpp
+++ quicksortalgoritma/quicksortalgoritma.cpp
@@ -74,3 +74,11 @@ void q_short(int low, int high)
 			if (i < j) //step 9
 			{
 				//swap the element at index i with the element at index j
+				swap(i, j);
+			}
+		}
+
+	if (low < j) { //step 11
+		//swap the pivot element with the element at index j
+		swap(low, j);
+	}
