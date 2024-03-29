0199d34055e10831a31e81abf4ce2f293f58f817
verss21 <arjunafrizal01@gmail.com>
2024-03-29 10:29:48 +0700

    step 7

--- quicksortalgoritma/quicksortalgoritma.cpp
+++ quicksortalgoritma/quicksortalgoritma.cpp
@@ -63,3 +63,6 @@ void q_short(int low, int high)
 				i++; //step 6
 				cmp_count++;
 			}
+			cmp_count++;
+			//search for an element less than or equal to pivot
+			while ((arr[j] > pivot) && (j > low)) //step 7
