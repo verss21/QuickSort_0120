7687af94a7e66c796f2cd8a955fb505abc909c90
verss21 <arjunafrizal01@gmail.com>
2024-03-29 10:14:18 +0700

    void input

--- quicksortalgoritma/quicksortalgoritma.cpp
+++ quicksortalgoritma/quicksortalgoritma.cpp
@@ -6,3 +6,27 @@ int arr[20];
 int cmp_count = 0; //number of comparasion
 int mov_count = 0; //number of data movements
 int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukan panjang element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+}
+{
+	cout << "\n--------------------" << endl;
+	cout << "nMasukan element array" << endl;
+	cout << "\n--------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
